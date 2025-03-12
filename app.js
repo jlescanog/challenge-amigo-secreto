6ec4f5b86810b236e24f3898ae97643e870a4fb4
jlescano-mobup <jlescano@mobuptacna.com>
2025-03-12 14:37:24 -0500

    agrega verificar vacios

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ let listaAmigos = [];
 
 function agregarAmigo() {
     let amigoIngresado = document.getElementById('amigo').value;
-
-    
+    //verificar si el campo está vacío
+    if  (amigoIngresado === '') {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        listaAmigos.push(amigoIngresado);
+        console.log(listaAmigos);   
+    };
 }
 
