2a2a118ed9aa4329293d41621969ca61a6c4d21b
jlescano-mobup <jlescano@mobuptacna.com>
2025-03-12 15:58:10 -0500

    crea funcion actualizarLista = <li>

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos = [];
+let amigos = [];
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById('amigo');
@@ -8,10 +8,20 @@ function agregarAmigo() {
     if (amigoIngresado === "") {  
         alert("Por favor, inserte un nombre.");
     } else {
-        listaAmigos.push(amigoIngresado);
-        console.log(listaAmigos);
+        amigos.push(amigoIngresado);
+        console.log(amigos);
         inputAmigo.value = "";  // Limpia el campo de entrada
-    }
+        actualizarLista();
+    };
 }
 
+function actualizarLista () {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; //limpia lista antes de agregar mas elementos
 
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
