51ce03abef9077b257bb7ee5fcc874df80a5810e
jlescano-mobup <jlescano@mobuptacna.com>
2025-03-12 14:29:22 -0500

    crea funcion captura valor

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigo() {
+    let amigoIngresado = document.getElementById('amigo').value;
+
+    
+}
+
